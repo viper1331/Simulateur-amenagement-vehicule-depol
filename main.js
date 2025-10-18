a03674af56b872fc9b10a5c5c074b3267a2f41e1
viper1331 <153918321+viper1331@users.noreply.github.com>
2025-10-18 22:28:59 +0200

    Add detailed logging for debugging

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const logger = (() => {
+  const prefix = '[Electron]';
+  const timestamp = () => new Date().toISOString();
+  const log = (method) => (message, details) => {
+    const formatted = `${prefix} ${timestamp()} - ${message}`;
+    if (details) {
+      console[method](formatted, details);
+    } else {
+      console[method](formatted);
+    }
+  };
+  return {
+    info: log('info'),
+    warn: log('warn'),
+    error: log('error')
+  };
+})();
+
 function createWindow() {
+  logger.info('Création d\'une nouvelle fenêtre principale');
   const win = new BrowserWindow({
     width: 1400,
     height: 900,
@@ -18,11 +37,19 @@ function createWindow() {
   });
 
   win.loadFile('index.html');
+  win.webContents.on('did-finish-load', () => {
+    logger.info('Fenêtre prête, contenu chargé');
+  });
+  win.on('closed', () => {
+    logger.info('Fenêtre principale fermée');
+  });
 }
 
 app.whenReady().then(() => {
+  logger.info('Application prête');
   createWindow();
   app.on('activate', () => {
+    logger.info('Activation de l\'application');
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -30,7 +57,9 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
+  logger.info('Toutes les fenêtres sont fermées');
   if (process.platform !== 'darwin') {
+    logger.info('Fermeture de l\'application');
     app.quit();
   }
 });
